9c35c12592bab5f3216aa2b51548a65eb5d0911d
CarinWood <carin.wood.85@gmail.com>
2024-01-16 10:36:45 +0100

    first commit

---
+++ school.js
@@ -0,0 +1,10 @@
+class School {
+    constructor(name) {
+        this.name = name
+        this.students = []
+        this.teachers = []
+        this.address
+        this.zipcode
+        this.city
+    }
+}
---
+++ teacher.js
@@ -0,0 +1,6 @@
+class Teacher {
+    constructor(name) {
+        this.name = name
+        this.subjects = []
+    }
+}
